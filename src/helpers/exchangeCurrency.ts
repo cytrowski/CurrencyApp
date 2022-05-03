0edda1cc19ec30d06ea117d4e88bee7feec440a3
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2022-05-03 22:29:44 +0200

    Use latest ratings
    For some dates the API does not return any values

--- src/helpers/exchangeCurrency.ts
+++ src/helpers/exchangeCurrency.ts
@@ -4,5 +4,5 @@ export enum CurrencyCode {
 }
 
 export const currencyFromToDates = (code: CurrencyCode): string => {
-  return `http://api.nbp.pl/api/exchangerates/rates/c/${code}/today?format=json`;
+  return `http://api.nbp.pl/api/exchangerates/rates/c/${code}/last?format=json`;
 };
